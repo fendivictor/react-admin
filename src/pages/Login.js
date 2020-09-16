82d72f5827656455860f2e942dc0d7e3b9e26839
user <user@DESKTOP-VMMC1CR>
2020-09-16 16:32:40 +0700

    Firebase added

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,7 @@
 import React, {Component} from 'react';
-import {Container, Row, Col, Form, Button} from 'react-bootstrap';
+import {Container, Row, Col} from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-import app from 'firebase/app';
-import 'firebase/auth';
+import firebase from 'firebase';
 
 const config = {
     apiKey: "AIzaSyCo2c_3Hbh7c-OFv0StO9gODQUPa-7MG04",
@@ -15,9 +13,26 @@ const config = {
     appId: "1:106491123919:web:f79341991787fb82957fba"
 };
 
+firebase.initializeApp(config);
+const provider = new firebase.auth.GoogleAuthProvider();
+
 class Login extends Component {
-    constructor() {
-        
+    state = {user: []}
+
+    onBtnLoginClick = () => {
+        firebase.auth().signInWithPopup(provider)
+        .then((res) => {
+            this.setState({user: res.user})
+        })
+        .catch((err) => {
+            console.log(err)
+        });
+    }
+
+    onBtnLogoutClick = () =>{
+        firebase.auth().signOut()
+        .then((res) => this.setState({user : []}))
+        .catch((err) => console.log(err))
     }
 
     render() {
@@ -27,26 +42,17 @@ class Login extends Component {
                     <Row className="login-form">
                         <Col>
                             <h2>Sign in</h2>
-                            <Form>
-                            <Form.Group controlId="formBasicEmail">
-                                <Form.Label>Email address</Form.Label>
-                                <Form.Control type="email" placeholder="Enter email" />
-                                <Form.Text className="text-muted">
-                                We'll never share your email with anyone else.
-                                </Form.Text>
-                            </Form.Group>
-
-                            <Form.Group controlId="formBasicPassword">
-                                <Form.Label>Password</Form.Label>
-                                <Form.Control type="password" placeholder="Password" />
-                            </Form.Group>
-                            <Form.Group controlId="formBasicCheckbox">
-                                <Form.Check type="checkbox" label="Check me out" />
-                            </Form.Group>
-                            <Button variant="primary" type="submit">
-                                Submit
-                            </Button>
-                            </Form>
+                            <div style={{display : 'flex' , justifyContent:'center',marginTop:'300px'}}>
+                            { this.state.user.length === 0
+                              ? <input type='button' value='login with google' onClick={this.onBtnLoginClick} />
+                              :
+                                <div>
+                                    <img src={this.state.user.photoURL  } alt='broken' width="200px"/>
+                                    <p>{this.state.user.displayName}</p>
+                                    <input type='button' value='log out' onClick={this.onBtnLogoutClick} />
+                                </div>
+                            }
+                            </div>
                         </Col>
                     </Row>
                 </Container>
